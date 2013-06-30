c4c9e622e3ffcab95943af470aefc5ef536ff4ca
Michael Hearne <mhearne@usgs.gov>
2013-06-29 18:31:34 -0600

    Fixed tests...

--- tests/test_pagerlib.py
+++ tests/test_pagerlib.py
@@ -5,13 +5,12 @@ from numpy import testing
 import numpy as np
 
 #local libraries
-from pagerlib import pagerio
-from pagerlib import pagerutil
+from pagerutil import colors,interp,matutil,text
 
 def test_colors():
     hexinput = '#FF00AA'
-    rgboutput = (1,0,0.39215686274509803)
-    output = pagerutil.colors.hex2rgb(hexinput)
+    rgboutput = (1,0,0.666666666)
+    output = colors.hex2rgb(hexinput)
     for pair in zip(rgboutput,output):
         testing.assert_almost_equal(pair[0],pair[1])
 
@@ -24,29 +23,41 @@ def test_interp():
     testing.assert_almost_equal(zout,zi)
 
 def test_repmat():
-    outmat = np.array([1,2,3,4,1,2,3,4])
-    inmat = np.array([1,2,3,4])
-    outmat2 = pagerutil.matutil.repmat(inmat,1,2)
+    outmat = np.array([1,2,3,4,1,2,3,4]).reshape(4,2,order='F')
+    inmat = np.ones((4,1))
+    inmat[0] = 1.
+    inmat[1] = 2.
+    inmat[2] = 3.
+    inmat[3] = 4.
+    outmat2 = matutil.repmat(inmat,1,2)
     testing.assert_almost_equal(outmat,outmat2)
 
 def test_sub2ind():
     idx = 5
-    mat = np.array([1,2,3,4,5,6,7,8,9]).reshape(3,3)
-    outidx = pagerutil.matutil.sub2ind(mat.shape,(2,2))
+    mat = np.random.rand(3,3)
+    outidx = matutil.sub2ind(mat.shape,(2,1))
     assert outidx == idx
 
 def test_ind2sub():
-    sub = (2,2)
-    mat = np.array([1,2,3,4,5,6,7,8,9]).reshape(3,3)
-    outsub = pagerutil.matutil.ind2sub(mat.shape,5)
-    assert outidx == idx
+    sub = (2,1)
+    mat = np.random.rand(3,3)
+    outsub = matutil.ind2sub(mat.shape,5)
+    assert outsub == sub
 
-def test_decToRoman(dec):
-    assert pagerutil.text.decToRoman(1025) == 'MXXV'
+def test_decToRoman():
+    assert text.decToRoman(1025) == 'MXXV'
 
 def test_setNumPrecision():
-    assert pagerutil.text.setNumPrecision(531,1) == 500
+    assert text.setNumPrecision(531,1) == 500
 
+if __name__ == '__main__':
+    test_colors()
+    test_interp()
+    test_repmat()
+    test_sub2ind()
+    test_ind2sub()
+    test_decToRoman()
+    test_setNumPrecision()    
 
 
 
